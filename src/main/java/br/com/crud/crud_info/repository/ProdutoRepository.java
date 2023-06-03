5af15237ad28236f66996ef9cf946e528d11967f
Alan Vitório <alanvitorio1985@gmail.com>
2023-06-03 02:19:47 -0300

    adicionando repository

--- src/main/java/br/com/crud/crud_info/repository/ProdutoRepository.java
+++ src/main/java/br/com/crud/crud_info/repository/ProdutoRepository.java
@@ -1,8 +1,9 @@
 package br.com.crud.crud_info.repository;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
-
+import java.util.Optional;
 import br.com.crud.crud_info.model.Produto;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,41 @@ public class ProdutoRepository {
   private Integer ultimoId = 0;
 
   public List<Produto> obterTodos() {
+    /** Metodo para retornar uma lista de produtos */
     return produtos;
+
   }
+
+  // encontra o produto por id
+  public Optional<Produto> obterPorId(Integer id) {
+    return produtos
+        .stream()
+        .filter(produto -> produto.getId() == id)
+        .findFirst();
+  }
+
+  public Produto adicionar(Produto produto) {
+
+    ultimoId++;
+    produto.setId(ultimoId);
+    produtos.add(produto);
+
+    return produto;
+  }
+
+  public void deletar(Integer id) {
+    produtos.removeIf(produto -> produto.getId() == id);
+  }
+
+  public Produto atualizar(Produto produto) {
+    Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+    if (produtoEncontrado.isEmpty()) {
+      throw new InputMismatchException("Produto não encontrado");
+    }
+    deletar(produto.getId());
+    produtos.add(produto);
+    return produto;
+
+  }
+
 }
